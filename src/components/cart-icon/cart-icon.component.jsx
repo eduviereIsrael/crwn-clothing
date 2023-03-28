35b99af08fc595f7eace2ddab34fbbed5f0f5de2
Eduviere Israel <eduviereisrael@gmail.com>
2023-03-28 12:28:05 +0200

    cart functionality added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,13 +5,13 @@ import { CartContext } from "../../contexts/cart.context";
 
 
 const CartIcon = () => {
-    const { setIsCartOpen, isCartOpen } = useContext(CartContext);
+    const { setIsCartOpen, isCartOpen, cartCount } = useContext(CartContext);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{cartCount}</span>
     </div>
   )
 }
